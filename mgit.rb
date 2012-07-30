d6158d41a4a56c9430d19bb7bea27c6d1bc5ba38
Felix C. Stegerman <flx@obfusk.net>
2012-07-30 20:44:59 +0200

    mgit: v0.1.1
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- mgit.rb
+++ mgit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mgit < Formula
   homepage 'https://github.com/obfusk/mgit'
-  url 'https://github.com/obfusk/mgit/tarball/v0.1.0'
-  sha1 '49d505775e6da6872a707d3e45e3225b4e368200'
+  url 'https://github.com/obfusk/mgit/tarball/v0.1.1'
+  sha1 'd7c0f58f4c14672fe18667741c90405e089099af'
 
   def install
     bin.install 'mgit'
