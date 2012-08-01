4ce421106646852dc4f1bfd34a060b576b404910
Felix C. Stegerman <flx@obfusk.net>
2012-08-01 21:20:41 +0200

    ocaml-camomile: 0.8.3 [new]
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

---
+++ ocaml-camomile.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class OcamlCamomile < Formula
+  homepage 'http://camomile.sourceforge.net'
+  url 'http://prdownloads.sourceforge.net/camomile/camomile-0.8.3.tar.bz2'
+  sha1 '9f2600010820689805b5ab6e4048217ea821d535'
+
+  depends_on 'objective-caml'
+  depends_on 'ocaml-findlib'
+
+  # Don't strip symbols, so dynamic linking doesn't break.
+  skip_clean :all
+
+  def install
+    ENV['OCAMLFIND_DESTDIR'] = lib+'ocaml/site-lib'
+    ENV.deparallelize
+
+    (lib+'ocaml/site-lib').mkpath
+
+    system './configure', "--prefix=#{prefix}"
+    system 'make'
+    system 'make install'
+  end
+end
